9b34d7f4037527d7ab59d62a0172d0e12c3cef65
vladislav <vipvladislav@gmail.com>
2020-10-01 12:07:10 +0300

    add Routs

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,5 +16,69 @@ class DefaultController extends AbstractController
         return $this->render('lucky/number.html.twig', ['number' => $aaa]);
     }
 
+//    /**
+//     * @param $aaa
+//     * @return Response
+//     * @Route("/{aaa}/blog", name="blog_list")
+//     */
+//    public function index($aaa)
+//    {
+//        $result =  $this->render('lucky/number.html.twig', ['number' => $aaa]);
+//        $this->addFlash('success', $aaa);
+//
+//        return $result;
+//    }
+
+    /**
+     * @Route("/{aaa}/blog_all.html.twig", name="blog_all.html.twig")
+     * @param $aaa
+     * @return Response
+     */
+    public function blogAll($aaa)
+    {
+        $result =  $this->render('lucky/blog_all.html.twig', ['number' => $aaa]);
+        $this->addFlash('success', $aaa);
+
+        return $result;
+    }
+
+    /**
+     * @Route("/{aaa}/news.html.twig", name="news.html.twig")
+     * @param $aaa
+     * @return Response
+     */
+    public function newsAll($aaa)
+    {
+        $result =  $this->render('lucky/news.html.twig', ['number' => $aaa]);
+        $this->addFlash('success', $aaa);
+
+        return $result;
+    }
+
+    /**
+     * @Route("/{aaa}/liturgy.html.twig", name="liturgy.html.twig")
+     * @param $aaa
+     * @return Response
+     */
+    public function liturgy($aaa)
+    {
+        $result =  $this->render(/** @lang text */ 'lucky/liturgy.html.twig', ['number' => $aaa]);
+        $this->addFlash('success', $aaa);
+
+        return $result;
+    }
+
+    /**
+     * @Route("/{aaa}/contact.html.twig", name="contact.html.twig")
+     * @param $aaa
+     * @return Response
+     */
+    public function contact($aaa)
+    {
+        $result =  $this->render(/** @lang text */ 'lucky/contact.html.twig', ['number' => $aaa]);
+        $this->addFlash('success', $aaa);
+
+        return $result;
+    }
 
 }
